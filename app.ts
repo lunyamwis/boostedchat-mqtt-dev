fb851686efb2f218cfb561fecf3ee9fa55402994
Brian Onang'o <surgbc@gmail.com>
2024-03-21 23:20:10 +0300

    Start server if regardless of state of logins

--- app.ts
+++ app.ts
@@ -4,13 +4,36 @@ import { MQTTListener } from "./services/mqttListener";
 import { SalesRepAccount } from "./services/receiveAccounts";
 
 export const initServers = async (salesRepAccounts: SalesRepAccount[]) => {
-  for (let i = 0; i < salesRepAccounts.length; i++) { // check this loop...,,, try with out own fields,,, if dev use a different server and endpoints...
-    //check proxy
-    await login(salesRepAccounts[i]);
-    const mqttListener = new MQTTListener(salesRepAccounts[i].igname);
-    mqttListener.registerRealtimeListeners();
-    await mqttListener.connectMQTTBroker();
+  const promises = [];
+  for (let i = 0; i < salesRepAccounts.length; i++) {
+    promises.push(initializeAccount(salesRepAccounts[i]));
   }
+
+  await Promise.allSettled(promises)
+    .then(results => {
+      const successfulInitializations = results.filter(result => result.status === 'fulfilled'); 
+
+      // Handle successful initializations
+      if (successfulInitializations.length > 0) {
+        console.log('At least some servers initialized successfully', successfulInitializations);
+      } else {
+        console.error('All server initializations failed!');
+      }
+
+      // Handle any failed initializations
+      const failedInitializations = results.filter(result => result.status === 'rejected');
+      failedInitializations.forEach(result => console.error('Initialization failed:', result));  
+    });
+
   const httpServer = new HttpServer();
   httpServer.initHttpServer();
 };
+
+
+
+async function initializeAccount(account:any) {
+  await login(account);
+  const mqttListener = new MQTTListener(account.igname);
+  mqttListener.registerRealtimeListeners();
+  await mqttListener.connectMQTTBroker();
+}
