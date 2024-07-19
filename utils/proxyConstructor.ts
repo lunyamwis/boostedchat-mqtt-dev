c35ef0aeb5810975168ebe25e6acf37fa484f7f9
lunyamwis <lutherlunyamwi@gmail.com>
2024-07-19 12:29:12 +0300

    encrypting

--- utils/proxyConstructor.ts
+++ utils/proxyConstructor.ts
@@ -1,5 +1,5 @@
 export const proxyConstructor = (country: string, city: string) => {
   // return `http://${Bun.env.PROXY_PASSWORD}:wifi;us;starlink;florida;miami+beach@proxy.soax.com:9000`
   // "http://Sql8t2uRG3XRvQrO:wifi;ke;starlink;nairobi;nairobi@proxy.soax.com:9000"
-  return `http://Sql8t2uRG3XRvQrO:wifi;ke;starlink;nairobi+county;nairobi@proxy.soax.com:9000`
+  return `http://${Bun.env.PROXY_PASSWORD}:wifi;ke;starlink;nairobi+county;nairobi@proxy.soax.com:9000`
 };
