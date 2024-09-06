4a12625dbd18ce4c78e5b58ed01fc9a6fbf28a65
dennis mokaya <anyoksdenn@gmail.com>
2024-09-06 05:30:45 +0300

    replace request with axios

--- src/entities/media.entity.ts
+++ src/entities/media.entity.ts
@@ -1,15 +1,26 @@
-import * as request from 'request-promise';
+import axios from 'axios';
 import { Entity } from '../core/entity';
 import { MediaEntityOembedResponse } from '../responses';
 
-export class MediaEntity extends Entity {
-  static async oembed(url: string): Promise<MediaEntityOembedResponse> {
-    return request({
-      url: 'https://api.instagram.com/instagram_oembed/',
-      json: true,
-      qs: {
-        url,
-      },
-    });
+  // export class MediaEntity extends Entity {
+  //   static async oembed(url: string): Promise<MediaEntityOembedResponse> {
+  //     return request({
+  //       url: 'https://api.instagram.com/instagram_oembed/',
+  //       json: true,
+  //       qs: {
+  //         url,
+  //       },
+  //     });
+  //   }
+  // }
+
+  export class MediaEntity extends Entity {
+    static async oembed(url: string): Promise<MediaEntityOembedResponse> {
+      const response = await axios.get('https://api.instagram.com/instagram_oembed/', {
+        params: {
+          url, // Axios uses `params` to handle query strings
+        },
+      });
+      return response.data; // Axios stores the result in `data` property
+    }
   }
-}
