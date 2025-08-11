4b4cfef6f438754b2b45bcf9741389f7f7564655
dennis mokaya <anyoksdenn@gmail.com>
2025-08-11 17:31:41 +0300

    add home to port

--- src/http-server/httpServer.ts
+++ src/http-server/httpServer.ts
@@ -37,7 +37,7 @@ export class HttpServer {
   }
 
   public initHttpServer() {
-    this.app.listen(3000, () => {
+    this.app.listen(3000, '0.0.0.0', () => {
       console.log('Server is running on port 3000');
     });
   }
